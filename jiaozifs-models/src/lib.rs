63142c8fb53cb77cc8387ee337ccf964ff50f2f1
ZhenYi <434836402@qq.com>
2024-09-14 21:11:34 +0800

    add models aksk and test

--- jiaozifs-models/src/lib.rs
+++ jiaozifs-models/src/lib.rs
@@ -1,3 +1,11 @@
 #![allow(non_upper_case_globals)]
+#![allow(unused_imports)]
 
 mod aksk;
+
+mod prelude{
+    pub use crate::aksk::{
+        AkSk,ListAkSkParams,DeleteAkSkParams,AkSkParams,AkSkRepo,
+    };
+
+}
