8084810a843e541885798956ded7dd96f4c5b252
ZhenYi <434836402@qq.com>
2024-12-10 21:51:54 +0800

    :label: part

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM ubuntu
 RUN apt-get update \
-  && apt-get install -y ca-certificates tzdata openssl\
+  && apt-get install -y ca-certificates tzdata openssl git\
   && rm -rf /var/lib/apt/lists/*
 
 WORKDIR /app
@@ -8,5 +8,5 @@ COPY  target/release/ControlPlane ./
 RUN mkdir "config"
 COPY config/config.toml ./config/
 
-EXPOSE 80
+EXPOSE 80, 2222
 CMD ["/app/ControlPlane"]
--- src/api/handler/owner/followers.rs
+++ src/api/handler/owner/followers.rs
@@ -1,45 +1,44 @@
-use std::string::ToString;
 use actix_session::Session;
-use actix_web::{web, Responder};
+use actix_web::web;
+use crate::api::ov::users::UserFollowerOv;
 use crate::api::service::Service;
-use crate::metadata::model::teams::teams;
-use crate::utils::r::R;
 
 #[utoipa::path(
     get,
     tag = "owner",
-    path = "/api/v1/owner/team",
+    path = "/api/v1/owner/follower",
     responses(
-            (status = 200, description = "Ok"),
+            (status = 200, description = "OK"),
             (status = 401, description = "Not Login"),
             (status = 402, description = "User Not Exist"),
+            (status = 403, description = "Get Followers Failed"),
             (status = 405, description = "Other Error"),
     ),
 )]
-pub async fn api_owner_team(
+pub async fn api_owner_follower(
     session: Session,
     service: web::Data<Service>
 )
--> impl Responder
+-> impl actix_web::Responder
 {
     let model = service.check.check_session(session).await;
-    if model.is_err() {
-        return R::<Vec<teams::Model>> {
+    if model.is_err(){
+        return crate::utils::r::R::<Vec<UserFollowerOv>>{
             code: 402,
             msg: Option::from("[Error] User Not Exist".to_string()),
             data: None,
         }
     }
-    match service.team.byuser(model.unwrap().uid).await {
-        Ok(x) => {
-            return R::<Vec<teams::Model>> {
+    match service.users.followers(model.unwrap().uid).await{
+        Ok(result)=>{
+            crate::utils::r::R::<Vec<UserFollowerOv>>{
                 code: 200,
-                msg: Option::from("[Success] Get Teams Success".to_string()),
-                data: Some(x),
+                msg: Option::from("[Ok]".to_string()),
+                data: Some(result),
             }
         },
-        Err(e) => {
-            R{
+        Err(e)=>{
+            crate::utils::r::R{
                 code: 405,
                 msg: Option::from(e.to_string()),
                 data: None,
